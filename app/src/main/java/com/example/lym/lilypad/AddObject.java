44500254ec8be9da31d583b7bbed2529883ae63c
Limyeongmin <ahkongbu@naver.com>
2016-11-16 15:10:48 +0900

    2016-11-16 광명시 이디아

--- app/src/main/java/com/example/lym/lilypad/AddObject.java
+++ app/src/main/java/com/example/lym/lilypad/AddObject.java
@@ -1,6 +1,10 @@
 package com.example.lym.lilypad;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
@@ -11,19 +15,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by LYM on 2016-11-10.
  */
-public class AddObject extends MainActivity {
+public class AddObject extends Activity {
     Button btnSave, btnCancel, btnInit, btnAsk;
     EditText etName;
     SoundPool sound;
     TextView result;
+    myDBHelper myHelper;
+    SQLiteDatabase sqlDB;
     int soundID[] = new int[7];
     final String[] Bell = {"벨소리를 선택하세요", "벨소리1", "벨소리2", "벨소리3", "벨소리4", "벨소리5", "벨소리6"};
     final Integer[] BellId = {null, R.raw.bell1, R.raw.bell2, R.raw.bell3, R.raw.bell4, R.raw.bell5, R.raw.bell6};
     static int selectedBell;
+
+
+   //db 생성
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context){
+            super(context, "BtnTBL", null, 1);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL("CREATE TABLE BtnTBL(etName CHAR(20) PRIMARY KEY);");
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("DROP TABLE IF EXISTS BtnTBL");
+            onCreate(db);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,8 +60,6 @@ public class AddObject extends MainActivity {
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
 
-        //DB 생성
-        final DBHelper dbHelper = new DBHelper(getApplicationContext(), "BTNINFO.db", null, 1);
 
         // 음악재생을 위해 사운드풀
         sound = new SoundPool(1, AudioManager.STREAM_RING, 0); // 동시재생수, 재생타입, 음악 품질(보통 0)
@@ -46,6 +67,7 @@ public class AddObject extends MainActivity {
         for (int i = 1; i < soundID.length; i++) {
             soundID[i] = sound.load(this, BellId[i], 1);
         }
+
         // DB 입력 결과를 확인하기 위한 텍스트뷰
 
 
@@ -67,21 +89,21 @@ public class AddObject extends MainActivity {
             }
         });
 
-
+        myHelper = new myDBHelper(this);
         btnSave.setOnClickListener(new View.OnClickListener() {
         @Override
            public void onClick(View v) {
-               String btnName = etName.getText().toString();
-               int bell = selectedBell;
+               //String btnName = etName.getText().toString();
+               //int bell = selectedBell;
 
-               dbHelper.insert(btnName, bell);
-            Intent intent = new Intent(AddObject.this, MainActivity.class);
-            startActivity(intent);
-
-               }
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO BtnTBL VALUES ('"+etName.getText().toString()+"');");
+                Toast.makeText(getApplicationContext(),"입력됨",Toast.LENGTH_SHORT).show();
+                sqlDB.close();
+                Intent intent = new Intent(AddObject.this, MainActivity.class);
+                startActivity(intent);
            }
-
-        );
+        });
 
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/lym/lilypad/MainActivity.java
+++ app/src/main/java/com/example/lym/lilypad/MainActivity.java
@@ -1,43 +1,54 @@
 package com.example.lym.lilypad;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnAdd;
+public class MainActivity extends AddObject {
+    Button btnAdd, btnChange;
     ListView listItem;
+    EditText newItem;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("타이틀을 입력하세요");
-
-
+        btnChange= (Button) findViewById(R.id.btnChange);
         btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
+        newItem = (EditText)findViewById(R.id.newItem);
 
-
-
-
-    }
-
-    // 버튼 클릭 이벤트를 처리하는 부분
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                Intent i = new Intent(this, AddObject.class);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddObject.class);
                 startActivity(i);
-                break;
+            }
+        });
 
-            default:
-                break;
-        }
+        btnChange.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT etName FROM BtnTBL;", null);
+                String strName = "이름" + "\r\n" + "-------" + "\r\n";
+                while(cursor.moveToNext()){
+
+                    strName += cursor.getString(0) + "\r\n";
+
+                }
+                newItem.setText(strName);
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+
+
 
     }
 
